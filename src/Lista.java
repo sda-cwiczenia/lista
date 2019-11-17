41dda74349231cd6d1a5783a7078e4bf93840ea1
Daniel Kazanecki <d.kazanecki85@gmail.com>
2019-11-17 10:37:33 +0100

    Drugi test

--- src/Lista.java
+++ src/Lista.java
@@ -11,9 +11,6 @@ public class Lista {
         System.out.println("7.Dawid Ruciński");
         System.out.println("8. Jeszcze jeden test");
         System.out.println("9. Mateusz Filipek");
-        System.out.println("9. Anna Ginter");
-
-        System.out.println("9. Jeszcze drugi test");
-
+        System.out.println("10. Anna Ginter");
     }
 }
