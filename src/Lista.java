a2cb8259d9da91fe5715d3f4856f7441b35f907d
Ireneusz Piątkowski <irek.piatkowski@gmail.com>
2019-11-17 09:33:11 +0100

    Dodany Irek Piątkowski

--- src/Lista.java
+++ src/Lista.java
@@ -2,5 +2,6 @@ public class Lista {
     public static void main(String[] args) {
         System.out.println("LISTA OBECNOŚCI");
         System.out.println("---------------");
+        System.out.println("1. Ireneusz Piątkowski");
     }
 }
